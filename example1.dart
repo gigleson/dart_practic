224c7ac0039514563b738fd17ee95859d7adc4e8
gigleson <kaushalranjit11@gmail.com>
2024-11-18 22:56:41 +0545

    new example

---
+++ example1.dart
@@ -0,0 +1,11 @@
+void main() {
+  int age = 25;
+  double height = 5.9;
+  String name = "Alice";
+  bool isStudent = true;
+
+  print('Name: $name');
+  print('Age: $age');
+  print('Height: $height');
+  print('Is Student: $isStudent');
+}
