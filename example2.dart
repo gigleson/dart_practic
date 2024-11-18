016199de4d65b3ba8fa6059aa43be7fec778dc49
gigleson <kaushalranjit11@gmail.com>
2024-11-18 22:58:10 +0545

    new example

---
+++ example2.dart
@@ -0,0 +1,11 @@
+void main() {
+  int number = 10;
+
+  if (number > 0) {
+    print('The number is positive');
+  } else if (number < 0) {
+    print('The number is negative');
+  } else {
+    print('The number is zero');
+  }
+}
